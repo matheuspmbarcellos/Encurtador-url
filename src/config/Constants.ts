c545f6c043091946868c35fbc1e5c61f2218f36f
Matheus Barcellos <matbarcellos21@gmail.com>
2022-07-18 21:59:17 -0300

    commit init

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	
+	MONGO_CONNECTION:
+	'mongodb+srv://matheuspmbarcellos:dev1234@url-shortener.yqanoqd.mongodb.net/url-shortener?retryWrites=true&w=majority',
+}
