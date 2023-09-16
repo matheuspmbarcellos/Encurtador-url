5f3eea61039968eec8b16c0bf92b185dd47eb025
Matheus Barcellos <100791632+matheuspmbarcellos@users.noreply.github.com>
2023-09-16 15:27:18 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -2,5 +2,4 @@ export const config = {
 	API_URL: 'http://localhost:5000',
 	
 	MONGO_CONNECTION:
-	'mongodb+srv://matheuspmbarcellos:<password>@url-shortener.yqanoqd.mongodb.net/url-shortener?retryWrites=true&w=majority',
 }
